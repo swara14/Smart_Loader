188100b169e7f258690cdf58bb77cb2a784ba7fe
shrutya22487 <shrutya22487@iiitd.ac.in>
2023-10-30 11:44:02 +0530

    update

--- loader.c
+++ loader.c
@@ -2,255 +2,217 @@
 #include <signal.h>
 Elf32_Ehdr *ehdr;
 Elf32_Phdr *phdr;
-//declaring global variables
-int fd , i ,min_entrypoint;
-Elf32_Addr entry_pt = 0 ;
+int fd, i, min_entrypoint;
+Elf32_Addr entry_pt = 0;
 void *virtual_mem = NULL;
+size_t PAGE_SIZE = 4096;
+void *entry_virtual;
+Elf32_Addr offset;
 
-void signal_handler( int signum ){
-  //allocating memory
-  if (signum == SIGSEGV) {
-        // Determine the faulting address
-        void *fault_addr = (void *)get_fault_address();
-
-        // Find the corresponding segment based on fault_addr and load it lazily
-        for (int i = 0; i < ehdr->e_phnum; i++) {
-            Elf32_Phdr *segment = &phdr[i];
-            if (fault_addr >= (void *)segment->p_vaddr &&
-                fault_addr < (void *)(segment->p_vaddr + segment->p_memsz)) {
-                // Calculate the offset within the segment
-                Elf32_Addr offset = (Elf32_Addr)fault_addr - segment->p_vaddr;
-                
-                //allocate memory when segmentation fault occurs
-                // Map a page-sized block of memory with read-write permissions
-                void *mapped_addr = mmap((void *)(segment->p_vaddr & ~(PAGE_SIZE - 1)),
-                                         PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-                if (mapped_addr == MAP_FAILED) {
-                    printf("Failed to allocate memory for lazy loading\n");
-                    exit(1);
-                }
-
-                // Copy the segment data to the mapped memory
-                memcpy(mapped_addr, (void *)(segment->p_offset + offset), PAGE_SIZE);
-
-                // Change protection to read-only to catch further accesses
-                mprotect(mapped_addr, PAGE_SIZE, PROT_READ);
-
-                return; // Segment loaded, no longer a segmentation fault
-            }
-        }
-
-        // If the faulting address is not within any segment, it's an error
-        printf("Segmentation fault at address: %p\n", fault_addr);
-        exit(1);
-    }
-}
-
-void setup_signal_handler() {
-    struct sigaction sh_sev;
-    memset(&sh_sev, 0, sizeof(sh_sev));
-    sh_sev.sa_handler = signal_handler; 
-    if (sigaction(SIGSEGV, &sh_sev, NULL) == -1)
-    {
-        printf("Error in handling SIGSEV\n");
-    }
-    memset(&sh_sev, 0, sizeof(sh_sev));
-}
-
-void free_space(){
-    free(ehdr);
-    free(phdr);
-}
-
-// Unmap virtual memory and close the file descriptor
-void unmapping_virtual_memory(){
-    if (virtual_mem != NULL) {
-        munmap(virtual_mem, phdr[i].p_memsz);
-    }
-    close(fd);
-}
-
-// Check if the ELF file can be opened for reading
-void check_file_read(const char* exe){
-  int fd = open(exe, O_RDONLY);
-  if (fd < 0) {
-    printf("Error opening ELF file\n");
-    exit(1);
-  }
-}
-// Check if offset seeking was successful
-void check_offset( off_t new_position ){
-  if ( new_position == -1 )
-  {
-    printf("Failed to seek offset\n");
-    exit(1);
-  }
-}
-
-// Load program headers into memory
-void load_phdr( size_t size_of_phdr ){
-  phdr = ( Elf32_Phdr* )malloc( size_of_phdr * ehdr->e_phnum); 
-  
-  if (phdr == NULL) {
-        printf("Failed to allocate memory for program headers.\n");
-        exit(1);
-  }
-  
-  check_offset(lseek(fd, 0, SEEK_SET));
-  check_offset( lseek(fd , ehdr -> e_phoff , SEEK_SET ) );
-  
-  // Read program headers into memory
-  if ( read( fd , phdr , size_of_phdr * ehdr -> e_phnum) !=  size_of_phdr * ehdr -> e_phnum)
-  {
-    printf("Failed to load program headers properly\n");
-    exit(1);
-  }
-  return;
-}
-
-// Load ELF header into memory and perform necessary checks
-void load_ehdr( size_t size_of_ehdr ){
-  ehdr = ( Elf32_Ehdr* )malloc(size_of_ehdr);
-  
-  if (ehdr == NULL) {
-        printf("Failed to allocate memory for ELF header.\n");
-        exit(1);
-  }
-
-  check_offset( lseek(fd, 0, SEEK_SET) ); 
-  // Read ELF header into memory
-  if (read(fd, ehdr, size_of_ehdr) != size_of_ehdr)
-  {
-    printf("Failed to load ELF header properly\n");
-    exit(1);
-  }
-  // Check if the ELF file is 32-bit
-  if (ehdr -> e_ident[EI_CLASS] != ELFCLASS32) {
-    printf("Not a 32-bit ELF file\n");
-    exit(1);
-  }
-  return;
-}
-
-/*
- * Load and run the ELF executable file
- */
-
-// Find the appropriate entry point in the program headers corres to PT_LOAD
-void find_entry_pt(){
-  i = 0  ;
-  min_entrypoint = 0;
-  int min = 0xFFFFFFFF;
-  for ( i = 0; i < ehdr -> e_phnum ; i++)
-  {
-    if ( phdr[i].p_flags == 0x5)
-    {
-      if (min > ehdr->e_entry - phdr[i].p_vaddr )
-      {
-        min = ehdr->e_entry - phdr[i].p_vaddr;
-        min_entrypoint = i;
-      }
-    }
-  }
-  i = min_entrypoint;
-  entry_pt = phdr[i].p_vaddr;
-}
-
-// Allocate virtual memory and load segment content
-void Load_memory(){
-  virtual_mem = mmap(NULL, phdr[i].p_memsz, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE , 0, 0);  
-  
-  if (virtual_mem == MAP_FAILED) {
-      printf("Failed to allocate virtual memory\n");
-      exit(1);
-  }
-  
-  check_offset(lseek(fd, 0, SEEK_SET));
-  check_offset( lseek( fd , phdr[i].p_offset ,SEEK_SET ) );
-
-  // Read segment content into virtual memory
-  read(fd , virtual_mem ,phdr[i].p_memsz) ;
-}
-
-// Open the ELF file and validate the file descriptor
-void open_elf( char* exe ){
-  fd = open(exe, O_RDONLY);
-  
-  if (fd < 0) {
-    printf("Failed to open ELF file\n");
-    exit(1);
-  }
-}
-
-// Load and execute the ELF executable
-void load_and_run_elf(char* exe) {
-  open_elf(exe);
-
-  size_t size_of_phdr = sizeof( Elf32_Phdr ) ,size_of_ehdr = sizeof( Elf32_Ehdr); // size of one program header
-
-  // 1. Load entire binary content into memory from the ELF file.
-  load_ehdr( size_of_ehdr );
-  load_phdr( size_of_phdr );
-
-  // 2. Find the appropriate entry point in the program headers
-  find_entry_pt();
-
-  // 3. Allocate memory and load the segment content
-  //Load_memory();
-
-  size_t page_size = 4096;  // Set page size to 4KB
-  Elf32_Addr total_memsz = phdr[i].p_memsz;
-  Elf32_Addr offset = ehdr->e_entry - entry_pt;
-
-// Iterate through the virtual memory addresses within the segment
-  for (Elf32_Addr addr = entry_pt; addr < entry_pt + total_memsz; addr += page_size) {
-    // Calculate the aligned address that is a multiple of the page size (page boundary)
-    void *mapped_addr = mmap((void *)(addr & ~(page_size - 1)),page_size, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-      // Check if the mmap call was successful
-    if (mapped_addr == MAP_FAILED) {
-        printf("Failed to allocate memory for segment\n");
-        exit(1);
-    }
-  }
-
-
-  // 4. Calculate the offset between entry point and segment starting address
-  Elf32_Addr offset = ehdr->e_entry - entry_pt;
-  // This should now be performed for the entire memory range
-    void *entry_virtual = virtual_mem + offset;
-    int (*_start)() = (int (*)())entry_virtual;
-    int result = _start();
-    printf("User _start return value = %d\n", result);
-}
-  // Get the actual memory address of the _start function
-  void *entry_virtual = virtual_mem + offset;
-
-  // 5. Typecast the address to a function pointer for "_start" method
-  int (*_start)() = (int(*)())entry_virtual;
-
-  // 6. Call the "_start" method and print the returned value
-  int result = _start();
-
-  // Cleanup and display result
-  printf("User _start return value = %d\n", result);
-
-
-int main(int argc, char** argv) 
+void check_offset(off_t new_position)
 {
-// checking if we get 2 arguments into the main
-  if(argc != 2) {
-    printf("Usage: %s <ELF Executable> \n",argv[0]);
-    exit(1);
-  }
-  // 1. Check the ELF file can be read
-  check_file_read(argv[1]);
+	if (new_position == -1)
+	{
+		printf("Failed to seek offset\n");
+		exit(1);
+	}
+}
 
-  // 2. Load and execute the ELF executable
-  load_and_run_elf(argv[1]);
+void Load_memory()
+{
+	virtual_mem = mmap(NULL, phdr[i].p_memsz, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE, 0, 0);
 
-  // 3. Perform cleanup
-  free_space();
-  unmapping_virtual_memory();
+	if (virtual_mem == MAP_FAILED)
+	{
+		printf("Failed to allocate virtual memory\n");
+		exit(1);
+	}
 
-  return 0;
+	check_offset(lseek(fd, 0, SEEK_SET));
+	check_offset(lseek(fd, phdr[i].p_offset, SEEK_SET));
+
+	// Read segment content into virtual memory
+	read(fd, virtual_mem, phdr[i].p_memsz);
+}
+
+
+void free_space()
+{
+	free(ehdr);
+	free(phdr);
+}
+
+void unmapping_virtual_memory()
+{
+	if (virtual_mem != NULL)
+	{
+		munmap(virtual_mem, phdr[i].p_memsz);
+	}
+	close(fd);
+}
+
+void check_file_read(const char *exe)
+{
+	int fd = open(exe, O_RDONLY);
+	if (fd < 0)
+	{
+		printf("Error opening ELF file\n");
+		exit(1);
+	}
+}
+
+void load_phdr(size_t size_of_phdr)
+{
+	phdr = (Elf32_Phdr *)malloc(size_of_phdr * ehdr->e_phnum);
+
+	if (phdr == NULL)
+	{
+		printf("Failed to allocate memory for program headers.\n");
+		exit(1);
+	}
+
+	check_offset(lseek(fd, 0, SEEK_SET));
+	check_offset(lseek(fd, ehdr->e_phoff, SEEK_SET));
+
+	// Read program headers into memory
+	if (read(fd, phdr, size_of_phdr * ehdr->e_phnum) != size_of_phdr * ehdr->e_phnum)
+	{
+		printf("Failed to load program headers properly\n");
+		exit(1);
+	}
+	return;
+}
+
+void load_ehdr(size_t size_of_ehdr)
+{
+	ehdr = (Elf32_Ehdr *)malloc(size_of_ehdr);
+
+	if (ehdr == NULL)
+	{
+		printf("Failed to allocate memory for ELF header.\n");
+		exit(1);
+	}
+
+	check_offset(lseek(fd, 0, SEEK_SET));
+	// Read ELF header into memory
+	if (read(fd, ehdr, size_of_ehdr) != size_of_ehdr)
+	{
+		printf("Failed to load ELF header properly\n");
+		exit(1);
+	}
+	// Check if the ELF file is 32-bit
+	if (ehdr->e_ident[EI_CLASS] != ELFCLASS32)
+	{
+		printf("Not a 32-bit ELF file\n");
+		exit(1);
+	}
+	return;
+}
+
+void find_entry_pt()
+{
+	i = 0;
+	min_entrypoint = 0;
+	int min = 0xFFFFFFFF;
+	for (i = 0; i < ehdr->e_phnum; i++)
+	{
+		if (phdr[i].p_flags == 0x5)
+		{
+			if (min > ehdr->e_entry - phdr[i].p_vaddr)
+			{
+				min = ehdr->e_entry - phdr[i].p_vaddr;
+				min_entrypoint = i;
+			}
+		}
+	}
+	i = min_entrypoint;
+	entry_pt = phdr[i].p_vaddr;
+}
+
+void open_elf(char *exe)
+{
+	fd = open(exe, O_RDONLY);
+
+	if (fd < 0)
+	{
+		printf("Failed to open ELF file\n");
+		exit(1);
+	}
+}
+
+void load_and_run_elf(char *exe)
+{
+	open_elf(exe);
+
+	size_t size_of_phdr = sizeof(Elf32_Phdr), size_of_ehdr = sizeof(Elf32_Ehdr); // size of one program header
+
+	load_ehdr(size_of_ehdr);
+	load_phdr(size_of_phdr);
+
+	find_entry_pt();
+
+	// Load_memory();
+	Elf32_Addr total_memsz = phdr[i].p_memsz;
+	Elf32_Addr offset = ehdr->e_entry - entry_pt;
+
+	offset = ehdr->e_entry - entry_pt;
+	entry_virtual = virtual_mem + offset;
+	int (*_start)() = (int (*)())entry_virtual;
+	int result = _start();
+	printf("User _start return value = %d\n", result);
+}
+
+void signal_handler(int signum)
+{
+	if (signum == SIGSEGV)
+	{
+		printf("GOT SIGSEV\n");
+		Load_memory();
+
+		Elf32_Addr offset = ehdr->e_entry - entry_pt;
+		entry_virtual = virtual_mem + offset;
+		int (*_start)() = (int (*)())entry_virtual;
+		int result = _start();
+		printf("User _start return value = %d\n", result);
+		// struct sigaction sh_sev;
+        // memset(&sh_sev, 0, sizeof(sh_sev));
+        // sh_sev.sa_handler = SIG_DFL;
+        // sigaction(SIGSEGV, &sh_sev, NULL);
+		free_space();
+		unmapping_virtual_memory();
+		return;
+	}
+}
+
+void setup_signal_handler()
+{
+	struct sigaction sh_sev;
+	memset(&sh_sev, 0, sizeof(sh_sev));
+	sh_sev.sa_handler = signal_handler;
+	if (sigaction(SIGSEGV, &sh_sev, NULL) == -1)
+	{
+		printf("Error in handling SIGSEV\n");
+	}
+	memset(&sh_sev, 0, sizeof(sh_sev));
+}
+
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		printf("Usage: %s <ELF Executable> \n", argv[0]);
+		exit(1);
+	}
+	setup_signal_handler();
+	// 1. Check the ELF file can be read
+	check_file_read(argv[1]);
+
+	// 2. Load and execute the ELF executable
+	load_and_run_elf(argv[1]);
+
+	// 3. Perform cleanup
+	
+
+	return 0;
 }
--- rough.c
+++ rough.c
@@ -1,124 +1,251 @@
 #include "loader.h"
+#include <signal.h>
 Elf32_Ehdr *ehdr;
 Elf32_Phdr *phdr;
+// declaring global variables
 int fd, i, min_entrypoint;
 Elf32_Addr entry_pt = 0;
 void *virtual_mem = NULL;
-void *loaded_segments = NULL; // Keep track of loaded segments
+size_t PAGE_SIZE = 4096;
+void *entry_virtual;
+Elf32_Addr offset;
 
-// Global variables to keep track of page faults and allocations
-int page_faults = 0;
-int page_allocations = 0;
-int internal_fragmentation = 0;
+void signal_handler(int signum)
+{
+	if (signum == SIGSEGV)
+	{
+		printf("GOT SIGSEV\n");
+		void *fault_addr = entry_virtual;
+		for (int i = 0; i < ehdr->e_phnum; i++)
+		{
+			Elf32_Phdr *segment = &phdr[i];
+			if (fault_addr >= (void *)segment->p_vaddr &&
+				fault_addr < (void *)(segment->p_vaddr + segment->p_memsz))
+			{
+				Elf32_Addr offset = (Elf32_Addr)fault_addr - segment->p_vaddr;
+				virtual_mem = mmap((void *)(segment->p_vaddr & ~(PAGE_SIZE - 1)),
+										 PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+				if (virtual_mem == MAP_FAILED)
+				{
+					printf("Failed to allocate memory for lazy loading\n");
+					exit(1);
+				}
 
-void free_space(){
-    free(ehdr);
-    free(phdr);
-    if (virtual_mem != NULL) {
-        free(loaded_segments);
-        munmap(virtual_mem, phdr[i].p_memsz);
-    }
-    close(fd);
+				// Copy the segment data to the mapped memory
+				memcpy(virtual_mem, (void *)(segment->p_offset + offset), PAGE_SIZE);
+
+				// Change protection to read-only to catch further accesses
+				mprotect(virtual_mem, PAGE_SIZE, PROT_READ);
+
+				//return; // Segment loaded, no longer a segmentation fault
+			}
+		}
+
+		// If the faulting address is not within any segment, it's an error
+		// printf("Segmentation fault at address: %p\n", fault_addr);
+		// exit(1);
+		Elf32_Addr offset = ehdr->e_entry - entry_pt;
+		entry_virtual = virtual_mem + offset;
+		int (*_start)() = (int (*)())entry_virtual;
+		int result = _start();
+		printf("User _start return value = %d\n", result);
+	}
 }
 
-
-void handle_page_fault(Elf32_Addr fault_addr) {
-    int segment_index = -1;
-    for (i = 0; i < ehdr->e_phnum; i++) {
-        if (phdr[i].p_type == PT_LOAD &&
-            fault_addr >= phdr[i].p_vaddr &&
-            fault_addr < phdr[i].p_vaddr + phdr[i].p_filesz) {
-            segment_index = i;
-            break;
-        }
-    }
-
-    if (segment_index == -1) {
-        printf("Invalid page fault address: 0x%08x\n", fault_addr);
-        exit(1);
-    }
-
-    // Calculate the page start and end addresses for the fault
-    Elf32_Addr page_start = phdr[segment_index].p_vaddr + (fault_addr - phdr[segment_index].p_vaddr) / PAGE_SIZE * PAGE_SIZE;
-    Elf32_Addr page_end = page_start + PAGE_SIZE;
-
-    // Allocate memory for the entire page
-    void *page_ptr = mmap(page_start, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, 0, 0);
-    if (page_ptr == MAP_FAILED) {
-        printf("Failed to allocate memory for page fault address: 0x%08x\n", fault_addr);
-        exit(1);
-    }
-
-    // Load the segment content into the allocated memory
-    check_offset(lseek(fd, 0, SEEK_SET));
-    check_offset(lseek(fd, phdr[segment_index].p_offset, SEEK_SET));
-    read(fd, page_ptr, phdr[segment_index].p_filesz);
-
-    // Update the loaded_segments to keep track of allocated pages
-    loaded_segments[segment_index] = page_ptr;
-    page_allocations++;
-
-    // Handle internal fragmentation
-    int internal_frag = PAGE_SIZE - phdr[segment_index].p_filesz;
-    if (internal_frag > 0) {
-        internal_fragmentation += internal_frag;
-    }
+void setup_signal_handler()
+{
+	struct sigaction sh_sev;
+	memset(&sh_sev, 0, sizeof(sh_sev));
+	sh_sev.sa_handler = signal_handler;
+	if (sigaction(SIGSEGV, &sh_sev, NULL) == -1)
+	{
+		printf("Error in handling SIGSEV\n");
+	}
+	memset(&sh_sev, 0, sizeof(sh_sev));
 }
 
-// Load and execute the ELF executable
-void load_and_run_elf(char* exe) {
-    open_elf(exe);
-
-    size_t size_of_phdr = sizeof(Elf32_Phdr), size_of_ehdr = sizeof(Elf32_Ehdr);
-
-    load_ehdr(size_of_ehdr);
-    load_phdr(size_of_phdr);
-
-    find_entry_pt();
-
-    // Allocate memory for the entry point segment
-    virtual_mem = mmap(NULL, phdr[i].p_memsz, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE, 0, 0);
-    loaded_segments = calloc(ehdr->e_phnum, sizeof(void*));
-
-    if (virtual_mem == MAP_FAILED) {
-        printf("Failed to allocate virtual memory\n");
-        exit(1);
-    }
-
-    // Calculate the offset between entry point and segment starting address
-    Elf32_Addr offset = ehdr->e_entry - entry_pt;
-    // Get the actual memory address of the _start function
-    void *entry_virtual = virtual_mem + offset;
-
-    // Typecast the address to a function pointer for "_start" method
-    int (*_start)() = (int(*)())entry_virtual;
-
-    // Use sigaction to catch page faults and handle them
-    struct sigaction sa;
-    sa.sa_sigaction = (void (*)(int, siginfo_t *, void *))handle_page_fault;
-    sa.sa_flags = SA_SIGINFO;
-    sigaction(SIGSEGV, &sa, NULL);
-
-    // Run the program
-    int result = _start();
-
-    // Cleanup and display results
-    printf("User _start return value = %d\n", result);
-    printf("Total Page Faults: %d\n", page_faults);
-    printf("Total Page Allocations: %d\n", page_allocations);
-    printf("Total Internal Fragmentation (KB): %d\n", internal_fragmentation);
-
-    free_space();
+void free_space()
+{
+	free(ehdr);
+	free(phdr);
 }
 
-int main(int argc, char** argv) {
-    if (argc != 2) {
-        printf("Usage: %s <ELF Executable> \n", argv[0]);
-        exit(1);
-    }
+void unmapping_virtual_memory()
+{
+	if (virtual_mem != NULL)
+	{
+		munmap(virtual_mem, phdr[i].p_memsz);
+	}
+	close(fd);
+}
 
-    check_file_read(argv[1]);
-    load_and_run_elf(argv[1);
+void check_file_read(const char *exe)
+{
+	int fd = open(exe, O_RDONLY);
+	if (fd < 0)
+	{
+		printf("Error opening ELF file\n");
+		exit(1);
+	}
+}
 
-    return 0;
+void check_offset(off_t new_position)
+{
+	if (new_position == -1)
+	{
+		printf("Failed to seek offset\n");
+		exit(1);
+	}
+}
+
+void load_phdr(size_t size_of_phdr)
+{
+	phdr = (Elf32_Phdr *)malloc(size_of_phdr * ehdr->e_phnum);
+
+	if (phdr == NULL)
+	{
+		printf("Failed to allocate memory for program headers.\n");
+		exit(1);
+	}
+
+	check_offset(lseek(fd, 0, SEEK_SET));
+	check_offset(lseek(fd, ehdr->e_phoff, SEEK_SET));
+
+	// Read program headers into memory
+	if (read(fd, phdr, size_of_phdr * ehdr->e_phnum) != size_of_phdr * ehdr->e_phnum)
+	{
+		printf("Failed to load program headers properly\n");
+		exit(1);
+	}
+	return;
+}
+
+void load_ehdr(size_t size_of_ehdr)
+{
+	ehdr = (Elf32_Ehdr *)malloc(size_of_ehdr);
+
+	if (ehdr == NULL)
+	{
+		printf("Failed to allocate memory for ELF header.\n");
+		exit(1);
+	}
+
+	check_offset(lseek(fd, 0, SEEK_SET));
+	// Read ELF header into memory
+	if (read(fd, ehdr, size_of_ehdr) != size_of_ehdr)
+	{
+		printf("Failed to load ELF header properly\n");
+		exit(1);
+	}
+	// Check if the ELF file is 32-bit
+	if (ehdr->e_ident[EI_CLASS] != ELFCLASS32)
+	{
+		printf("Not a 32-bit ELF file\n");
+		exit(1);
+	}
+	return;
+}
+
+void find_entry_pt()
+{
+	i = 0;
+	min_entrypoint = 0;
+	int min = 0xFFFFFFFF;
+	for (i = 0; i < ehdr->e_phnum; i++)
+	{
+		if (phdr[i].p_flags == 0x5)
+		{
+			if (min > ehdr->e_entry - phdr[i].p_vaddr)
+			{
+				min = ehdr->e_entry - phdr[i].p_vaddr;
+				min_entrypoint = i;
+			}
+		}
+	}
+	i = min_entrypoint;
+	entry_pt = phdr[i].p_vaddr;
+}
+
+void Load_memory()
+{
+	virtual_mem = mmap(NULL, phdr[i].p_memsz, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE, 0, 0);
+
+	if (virtual_mem == MAP_FAILED)
+	{
+		printf("Failed to allocate virtual memory\n");
+		exit(1);
+	}
+
+	check_offset(lseek(fd, 0, SEEK_SET));
+	check_offset(lseek(fd, phdr[i].p_offset, SEEK_SET));
+
+	// Read segment content into virtual memory
+	read(fd, virtual_mem, phdr[i].p_memsz);
+}
+
+void open_elf(char *exe)
+{
+	fd = open(exe, O_RDONLY);
+
+	if (fd < 0)
+	{
+		printf("Failed to open ELF file\n");
+		exit(1);
+	}
+}
+
+void load_and_run_elf(char *exe)
+{
+	open_elf(exe);
+
+	size_t size_of_phdr = sizeof(Elf32_Phdr), size_of_ehdr = sizeof(Elf32_Ehdr); // size of one program header
+
+	load_ehdr(size_of_ehdr);
+	load_phdr(size_of_phdr);
+
+	find_entry_pt();
+
+	// Load_memory();
+	Elf32_Addr total_memsz = phdr[i].p_memsz;
+	Elf32_Addr offset = ehdr->e_entry - entry_pt;
+
+	// Iterate through the virtual memory addresses within the segment
+	for (Elf32_Addr addr = entry_pt; addr < entry_pt + total_memsz; addr += PAGE_SIZE)
+	{
+		// Calculate the aligned address that is a multiple of the page size (page boundary)
+		void *virtual_mem = mmap((void *)(addr & ~(PAGE_SIZE - 1)), PAGE_SIZE, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+		// Check if the mmap call was successful
+		if (virtual_mem == MAP_FAILED)
+		{
+			printf("Failed to allocate memory for segment\n");
+			exit(1);
+		}
+	}
+
+	offset = ehdr->e_entry - entry_pt;
+	entry_virtual = virtual_mem + offset;
+	int (*_start)() = (int (*)())entry_virtual;
+	int result = _start();
+	printf("User _start return value = %d\n", result);
+}
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		printf("Usage: %s <ELF Executable> \n", argv[0]);
+		exit(1);
+	}
+	setup_signal_handler();
+	// 1. Check the ELF file can be read
+	check_file_read(argv[1]);
+
+	// 2. Load and execute the ELF executable
+	load_and_run_elf(argv[1]);
+
+	// 3. Perform cleanup
+	free_space();
+	unmapping_virtual_memory();
+
+	return 0;
 }
