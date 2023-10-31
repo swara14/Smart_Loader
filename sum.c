2a9bde1504c0ab8932b32fab44891ebeea57ba79
shrutya22487 <shrutya22487@iiitd.ac.in>
2023-10-31 21:49:41 +0530

    update

--- sum.c
+++ sum.c
@@ -1,8 +1,7 @@
 #define SIZE 1024
-//int __attribute__((aligned(4096))) ;
+int __attribute__((aligned(4096))) A[SIZE] = { 0 };
 int sum = 0;
 int _start() {
-  int A[SIZE] = { 0 };
   for (int i = 0; i < SIZE; i++) A[i] = 2;
   for (int i = 0; i < SIZE; i++)
     sum += A[i];
