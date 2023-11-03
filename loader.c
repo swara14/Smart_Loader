6d393bca54cea16e891742c94b7971b5a17c63d2
Swara Parekh <132156240+swara14@users.noreply.github.com>
2023-11-03 10:42:09 +0530

    Update loader.c for bonus

--- loader.c
+++ loader.c
@@ -88,38 +88,111 @@ void open_elf( char* exe ){
   }
 }
 
-void segfault_handler(int signum, siginfo_t *info, void *context) {
-  if (signum == SIGSEGV)
-  {
-    no_of_faults++;
-    off_t offset = 0;
-    size_t size_to_be_allocated = 0;
+// void segfault_handler(int signum, siginfo_t *info, void *context) {
+//   if (signum == SIGSEGV)
+//   {
+//     no_of_faults++;
+//     off_t offset = 0;
+//     size_t size_to_be_allocated = 0;
 
-    for (int i = 0; i < ehdr->e_phnum ; i++)
-    {
-      if (phdr[i].p_type == PT_LOAD)
-      {
-        if ( (info->si_addr) >= (phdr[i].p_vaddr) && (info->si_addr) < phdr[i].p_vaddr + phdr[i].p_memsz ){
+//     for (int i = 0; i < ehdr->e_phnum ; i++)
+//     {
+//       if (phdr[i].p_type == PT_LOAD)
+//       {
+//         if ( (info->si_addr) >= (phdr[i].p_vaddr) && (info->si_addr) < phdr[i].p_vaddr + phdr[i].p_memsz ){
 
-          size_to_be_allocated = roundup(phdr[i].p_memsz);
-          total_size += size_to_be_allocated;
-          fragmentation = fragmentation + size_to_be_allocated - phdr[i].p_memsz;
-          //printf("fragmentation now is :%d\n", size_to_be_allocated - phdr[i].p_memsz);
-          virtual_mem = mmap( info -> si_addr , size_to_be_allocated , PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE , 0, 0);  
-          check_offset(lseek(fd, phdr[i].p_offset , SEEK_SET) );
-          read(fd, virtual_mem  , phdr[i].p_memsz);
-          break; 
-        }
-      }      
-    }
-  }
-}
+//           size_to_be_allocated = roundup(phdr[i].p_memsz);
+//           total_size += size_to_be_allocated;
+//           fragmentation = fragmentation + size_to_be_allocated - phdr[i].p_memsz;
+//           //printf("fragmentation now is :%d\n", size_to_be_allocated - phdr[i].p_memsz);
+//           virtual_mem = mmap( info -> si_addr , size_to_be_allocated , PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE , 0, 0);  
+//           check_offset(lseek(fd, phdr[i].p_offset , SEEK_SET) );
+//           read(fd, virtual_mem  , phdr[i].p_memsz);
+//           break; 
+//         }
+//       }      
+//     }
+//   }
+// }
 
-void setup_signal_handler(){
+// Array to track allocated pages within a segment
+char *page_bitmap = NULL;
+
+void setup_signal_handler() {
     struct sigaction sa;
     sa.sa_flags = SA_SIGINFO;
     sa.sa_sigaction = segfault_handler;
     sigaction(SIGSEGV, &sa, NULL);
+}
+
+// Initialize the page bitmap based on the size of the segment
+void initialize_page_bitmap(Elf32_Phdr phdr) {
+    // Calculate the number of pages required for the segment
+    size_t num_pages = (phdr.p_memsz + PAGE_SIZE - 1) / PAGE_SIZE;
+    page_bitmap = (char *)calloc(num_pages, sizeof(char));
+    if (page_bitmap == NULL) {
+        printf("Failed to allocate page_bitmap\n");
+        exit(1);
+    }
+}
+
+// Update the page bitmap to mark a page as allocated when a page fault occurs
+void update_page_bitmap(Elf32_Phdr phdr, void *addr) {
+    // Calculate the page number for the faulting address
+    size_t page_offset = (size_t)(addr - phdr.p_vaddr);
+    size_t page_number = page_offset / PAGE_SIZE;
+    page_bitmap[page_number] = 1;
+}
+
+
+void segfault_handler(int signum, siginfo_t *info, void *context) {
+    if (signum == SIGSEGV) {
+        no_of_faults++;
+        for (int i = 0; i < ehdr->e_phnum; i++) {
+            if (phdr[i].p_type == PT_LOAD) {
+                if ((info->si_addr) >= (phdr[i].p_vaddr) && (info->si_addr) < phdr[i].p_vaddr + phdr[i].p_memsz) {
+                    if (page_bitmap == NULL) {
+                        initialize_page_bitmap(phdr[i]);
+                    }
+                    update_page_bitmap(phdr[i], info->si_addr);
+
+                    // Check if all pages within the segment are allocated
+                    size_t num_pages = (phdr[i].p_memsz + PAGE_SIZE - 1) / PAGE_SIZE;
+                    int all_pages_allocated = 1;
+                    for (size_t j = 0; j < num_pages; j++) {
+                        if (page_bitmap[j] == 0) {
+                            all_pages_allocated = 0;
+                            break;
+                        }
+                    }
+
+                    if (all_pages_allocated) {
+                        // All pages within the segment are allocated, nothing to do
+                        return;
+                    }
+
+                    // Calculate the page offset within the segment
+                    size_t page_offset = (size_t)(info->si_addr - phdr[i].p_vaddr);
+                    size_t page_number = page_offset / PAGE_SIZE;
+
+                    // Calculate the size for the page to be allocated (usually 4 KB)
+                    size_t size_to_be_allocated = PAGE_SIZE;
+
+                    // Calculate the address for the page to be mapped
+                    void *page_addr = (void *)(phdr[i].p_vaddr + page_number * PAGE_SIZE);
+
+                    // Map a single page at a time
+                    virtual_mem = mmap(page_addr, size_to_be_allocated, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE, 0, 0);
+
+                    // Set the file offset to the beginning of the page within the segment
+                    check_offset(lseek(fd, phdr[i].p_offset + page_number * PAGE_SIZE, SEEK_SET));
+
+                    // Read data from the ELF file into the mapped page
+                    read(fd, virtual_mem, size_to_be_allocated);
+                }
+            }
+        }
+    }
 }
 
 void load_and_run_elf(char* exe) {
